5a760c439b806dd54c05540ca56a34039db294ce
recruiterclub88-cmd <recruiterclub88@gmail.com>
2026-02-11 23:47:11 +0100

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 # Start both Next.js and Baileys worker
 
+# Set NODE_PATH to find modules in standalone build
+export NODE_PATH=/app/node_modules:/app/.next/server/node_modules
+
 # Start Next.js in background
 node server.js &
 
